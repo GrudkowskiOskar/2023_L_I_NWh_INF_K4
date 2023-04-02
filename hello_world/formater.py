79d1247295b90f3d46d82cac88f823b6e3eee8a6
GrudkowskiOskar <wrx81747@student.wsb.wroclaw.pl>
2023-04-02 16:02:24 +0200

    Zmiana imienia

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -7,31 +7,31 @@ JSON = "json"
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
 
-def get_formatted(msg, imie, format):
+def get_formatted(msg, Oskar, format):
     result = ""
     if format == PLAIN:
-        result = plain_text(msg, imie)
+        result = plain_text(msg, Oskar)
     elif format == PLAIN_UP:
-        result = plain_text_upper_case(msg, imie)
+        result = plain_text_upper_case(msg, Oskar)
     elif format == PLAIN_LO:
-        result = plain_text_lower_case(msg, imie)
+        result = plain_text_lower_case(msg, Oskar)
     elif format == JSON:
-        result = format_to_json(msg, imie)
+        result = format_to_json(msg, Oskar)
     return result
 
 
-def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
+def format_to_json(msg, Oskar):
+    return ('{ "imie":"' + Oskar + '", "mgs":' +
             msg + '"}')
 
 
-def plain_text(msg, imie):
+def plain_text(msg, Oskar):
     return imie + ' ' + msg
 
 
-def plain_text_upper_case(msg, imie):
+def plain_text_upper_case(msg, Oskar):
     return plain_text(msg.upper(), imie.upper())
 
 
-def plain_text_lower_case(msg, imie):
+def plain_text_lower_case(msg, Oskar):
     return plain_text(msg.lower(), imie.lower())
